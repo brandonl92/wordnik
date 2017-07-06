fde0587863f0adbbcb2a1bb2f45458e9da071af2
chtrinh <chris.chtrinh@gmail.com>
2017-07-06 14:07:50 -0700

    Add comment for api

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,15 @@
+
+/*
+This is a client for Wordnik REST API.
+
+REF: http://developer.wordnik.com/docs.html
+*/
 class Api {
   water() {
     console.log('hello');
   }
+
+
 }
 
 export default Api;
